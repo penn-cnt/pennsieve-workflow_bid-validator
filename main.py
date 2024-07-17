624a34b421c21afa8a3f1920802e49fe58b0cac0
UPennBJPrager <bjprager@seas.upenn.edu>
2024-07-17 13:13:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import shutil
 import datetime
 
 # Define import and export directories via environment variables (required by pennsieve as of 041924)
-src  = 'data/input/BIDS'
-dest = 'data/output/'
+src  = os.environ['INPUT_DIR']+'/BIDS/'
+dest = os.environ['OUTPUT_DIR']+'/'
 
 # Get a date time for the report
 current_time = datetime.datetime.now()
 outfile      = f"bidsreport_{current_time.month:02d}{current_time.day:02d}{current_time.year:04d}_{current_time.hour:02d}{current_time.minute:02d}.report"
 
 # Run the command
-os.system(f"bids-validator {src} > {dest}/{outfile}")
+os.system(f"bids-validator {src} > {dest}{outfile}")
